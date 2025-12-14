b526d85841bac35a36c1ef227d821031b29b148c
Anandh <98966404+WedDesign2904@users.noreply.github.com>
2025-12-14 12:21:46 +0530

    Add files via upload

---
+++ SQL Window Function.sql
@@ -0,0 +1,13 @@
+-- SQL Window Function 
+
+SELECT
+    *
+  FROM Sales.Orders
+
+/*
+ALL Window Function Using Data Analysis
+*/
+
+-- To be Continous
+
+https://github.com/WedDesign2904/Advanced-SQL-Data-Analysis-Window-Aggregate-Functions-
